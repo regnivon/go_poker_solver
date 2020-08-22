0af8311c08cea8159d61e888484b58363fbf9af4
regnivon <qdnovinger@gmail.com>
2020-08-22 16:33:14 -0400

    testing changes

--- driver/main.go
+++ driver/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/chehsunliu/poker"
 	"log"
 	"os"
 	"regnivon/solv"
 	"runtime/pprof"
-	"fmt"
 )
 
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
@@ -29,6 +29,8 @@ func main() {
 	ipHands := "KQs, QJs, JTs, T9s, 98s, 77+"
 	board := []poker.Card{poker.NewCard("Ac"), poker.NewCard("7s"), poker.NewCard("5s"),
 		poker.NewCard("3d"), poker.NewCard("2h")}
+
+
 
 	//oopHands := "AA, KK, QQ, JJ, TT, 99, 88, 77, 66, 55, 44, 33, 22, AK, AQ, AJ, AT, A9, A8, A7, A6, A5, A4, A3, A2, KQ, KJ, KT, K9, K8, K7, K6, K5, K4, K3, K2, QJ, QT, Q9, Q8, Q7, Q6, Q5, Q4, Q3, Q2, JT, J9, J8, J7, J6, J5, J4, J3, J2, T9, T8, T7, T6, T5, T4, T3, T2, 98, 97, 96, 95, 94, 93, 92, 87, 86, 85, 84, 83, 82, 76, 75, 74, 73, 72, 65, 64, 63, 62, 54, 53, 52, 43, 42, 32"
 	//ipHands := "AA, KK, QQ, JJ, TT, 99, 88, 77, 66, 55, 44, 33, 22, AK, AQ, AJ, AT, A9, A8, A7, A6, A5, A4, A3, A2, KQ, KJ, KT, K9, K8, K7, K6, K5, K4, K3, K2, QJ, QT, Q9, Q8, Q7, Q6, Q5, Q4, Q3, Q2, JT, J9, J8, J7, J6, J5, J4, J3, J2, T9, T8, T7, T6, T5, T4, T3, T2, 98, 97, 96, 95, 94, 93, 92, 87, 86, 85, 84, 83, 82, 76, 75, 74, 73, 72, 65, 64, 63, 62, 54, 53, 52, 43, 42, 32"
@@ -60,24 +62,4 @@ func main() {
 	solv.OutputTree(tree, 0)
 	traversal := solv.NewTraversal(oop, ip)
 	solv.Train(traversal, 1000, tree)
-
-
-	average := tree.GetAverageStrategy()
-	average2 := tree.GetNext(0).GetNext(1).(*solv.GameNode).GetAverageStrategy()
-
-	for hand := range oop {
-		fmt.Println(oop[hand].Hand, average[hand])
-	}
-
-fmt.Println("\n\nnext\n\n")
-	for hand := range oop {
-//fmt.Println(oop[hand].Hand, average[hand])		
-	fmt.Println(oop[hand].Hand, tree.GetNext(0).GetNext(1).(*solv.GameNode).Strategy(hand), average2[hand])
-	}
-
-	//fmt.Printf("%v\n", average)
-	//fmt.Printf("%v\n", average2)
-	//fmt.Printf("%v %v\n", len(oop), len(ip)) 
-	fmt.Println(tree.GetNext(0).GetNext(1).(*solv.GameNode).PotSize()) 
-
 }
